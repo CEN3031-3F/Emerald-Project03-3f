d2ba9ddc9fe84ea40a3e97da9aeb76a94ce31092
Dany Rashwan <dannirash@outlook.com>
2023-11-06 19:15:08 -0500

    some styling

--- client/src/views/Assessment/StudentAssessment/StudentAssessment.jsx
+++ client/src/views/Assessment/StudentAssessment/StudentAssessment.jsx
@@ -35,28 +35,29 @@ function Assessment() {
   return (
     <div className='container flex-row nav-padding'>
         <NavBar isStudent={true} />
-        <div className="Assessment">
-        <div className="diagonal-buttons">
-            {questions.map((question, index) => (
-            <button className="question-button" onClick={() => openPopup(index)}>Question {index + 1}</button>
-            ))}
-        </div>
-        {questions.map((question, index) => (
-            <div key={index} className={`popup ${showPopups[index] ? 'active' : ''}`}>
-            <div className="popup-content">
-                <div>{question.text}</div>
-                <div className="options">
-                {question.options.map((option, optionIndex) => (
-                    <div key={optionIndex}>
-                    <input type="radio" name={`question-${index}`} id={`question-${index}-option-${optionIndex}`} value={option} />
-                    <label htmlFor={`question-${index}-option-${optionIndex}`}>{option}</label>
-                    </div>
+        <div className="big-text">Assessment 1</div>
+            <div className="Assessment">
+            <div className="diagonal-buttons">
+                {questions.map((question, index) => (
+                <button className="question-button" onClick={() => openPopup(index)}>Question {index + 1}</button>
                 ))}
+            </div>
+            {questions.map((question, index) => (
+                <div key={index} className={`popup ${showPopups[index] ? 'active' : ''}`}>
+                <div className="popup-content">
+                    <div>{question.text}</div>
+                    <div className="options">
+                    {question.options.map((option, optionIndex) => (
+                        <div key={optionIndex}>
+                        <input type="radio" name={`question-${index}`} id={`question-${index}-option-${optionIndex}`} value={option} />
+                        <label htmlFor={`question-${index}-option-${optionIndex}`}>{option}</label>
+                        </div>
+                    ))}
+                    </div>
+                    <button onClick={() => closePopup(index)}>Close</button>
                 </div>
-                <button onClick={() => closePopup(index)}>Close</button>
-            </div>
-            </div>
-        ))}
+                </div>
+            ))}
         </div>
     </div>
   );

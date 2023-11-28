78981274a9cba44b6487dbdb66ca0682e2b0b69e
Dany Rashwan <61055337+dannirash@users.noreply.github.com>
2023-11-27 23:13:37 -0500

    Assessment view

--- client/src/views/Assessment/Assessment.jsx
+++ client/src/views/Assessment/Assessment.jsx
@@ -3,78 +3,78 @@ import './Assessment.css';
 import { getAssessmentQuestions } from '../../Utils/requests';
 
 function Assessment(props) {
-
-  // The old questions array that was created statically had fields of text and options.
-  // TODO: get the questions from the database and loop through them, assigning questiontext to text, and each option to options based on the type.
-  // First: I need to get the assessments.jsx in the mentor view assessments table, so that the page is assigned an authentication token and I can make requests here.
-  // Basically, I need to get an open questions button for each assessment.
-
-  
   const [questions, setQuestions] = useState([]);
-  const {assessmentId} = props;
+  const [answers, setAnswers] = useState([]);
+  const { assessmentId } = props;
 
   useEffect(() => {
-
-    // get dbresponse with questions.
-    const fetchData = async() => {
-      let dbresponse;
-      dbresponse = await getAssessmentQuestions(assessmentId);
-      const newQuestions = dbresponse.data.questions.map((item, index) => 
-      {
-        const opt1 = item.Choice_1_text;
-        const opt2 = item.Choice_2_text;
-        const opt3 = item.Choice_3_text;
-        const opt4 = item.Choice_4_text;
-
-        const options = [opt1, opt2, opt3, opt4];
+    const fetchData = async () => {
+      let dbresponse = await getAssessmentQuestions(assessmentId);
+      const newQuestions = dbresponse.data.questions.map(item => {
+        let type, options;
+        if (item.Choice_1_text === 'true' && item.Choice_2_text === 'false') {
+          type = 'trueFalse';
+          options = ['true', 'false'];
+        } else if (!item.Choice_1_text && !item.Choice_2_text && !item.Choice_3_text && !item.Choice_4_text) {
+          type = 'openResponse';
+          options = [];
+        } else {
+          type = 'multipleChoice';
+          options = [item.Choice_1_text, item.Choice_2_text, item.Choice_3_text, item.Choice_4_text].filter(Boolean);
+        }
 
         return {
-          text : item.Question_text,
-          options : options,
+          text: item.Question_text,
+          options: options,
+          type: type
         };
       });
       setQuestions(newQuestions);
+      setAnswers(Array(newQuestions.length).fill(''));
     };
     fetchData();
-  }, []);
+  }, [assessmentId]);
 
-  const [showPopups, setShowPopups] = useState(Array(questions.length).fill(false));
-
-  const openPopup = (index) => {
-    const newPopups = [...showPopups];
-    newPopups[index] = true;
-    setShowPopups(newPopups);
+  const handleAnswerChange = (index, value) => {
+    const newAnswers = [...answers];
+    newAnswers[index] = value;
+    setAnswers(newAnswers);
   };
 
-  const closePopup = (index) => {
-    const newPopups = [...showPopups];
-    newPopups[index] = false;
-    setShowPopups(newPopups);
+  const submitAnswers = () => {
+    // Logic to submit answers
+    console.log(answers);
   };
 
   return (
     <div className="Assessment">
-      <div className="diagonal-buttons">
-        {questions.map((question, index) => (
-          <button className="question-button" onClick={() => openPopup(index)}>Question {index + 1}</button>
-        ))}
-      </div>
       {questions.map((question, index) => (
-        <div key={index} className={`popup ${showPopups[index] ? 'active' : ''}`}>
-          <div className="popup-content">
-            <div>{question.text}</div>
+        <div key={index} className="student-question">
+          <div className="question-text">{index + 1}. {question.text}</div>
+          {question.type === 'openResponse' ? (
+            <textarea 
+              value={answers[index]}
+              onChange={(e) => handleAnswerChange(index, e.target.value)}
+            />
+          ) : (
             <div className="options">
               {question.options.map((option, optionIndex) => (
-                <div key={optionIndex}>
-                  <input type="radio" name={`question-${index}`} id={`question-${index}-option-${optionIndex}`} value={option} />
-                  <label htmlFor={`question-${index}-option-${optionIndex}`}>{option}</label>
-                </div>
+                <label key={optionIndex}>
+                  <input
+                    type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
+                    name={`question-${index}`}
+                    value={option}
+                    checked={answers[index] === option}
+                    onChange={() => handleAnswerChange(index, option)}
+                  />
+                  {String.fromCharCode(97 + optionIndex)}) {option}
+                </label>
               ))}
             </div>
-            <button onClick={() => closePopup(index)}>Close</button>
-          </div>
+          )}
         </div>
       ))}
+      <button onClick={submitAnswers}>Submit Answers</button>
     </div>
   );
 }

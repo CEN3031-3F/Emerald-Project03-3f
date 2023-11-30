302857e822734ca8cd8b19f67d6a24ea4c689ec5
Dany Rashwan <dannirash@outlook.com>
2023-11-30 16:06:14 -0500

    Student view Toggle
    successfuly implemented

--- client/src/views/Assessment/Assessment.jsx
+++ client/src/views/Assessment/Assessment.jsx
@@ -1,84 +1,159 @@
 import React, { useState, useEffect } from 'react';
 import './Assessment.css';
+import Switch from '../../components/NavBar/Switch';
+import './StudentAssessment/StudentAssessment.css';
 import { getAssessmentQuestions } from '../../Utils/requests';
 
-function Assessment(props) {
+function Assessment({ assessmentId }) {
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
-  const { assessmentId } = props;
+  const [isOn, setIsOn] = useState(true);
+  const [showPopups, setShowPopups] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log('props');
-      console.log(props);
-      let dbresponse = await getAssessmentQuestions(assessmentId);
-      const newQuestions = dbresponse.data.questions.map(item => {
-        let type, options;
-        if (item.Choice_1_text === 'true' && item.Choice_2_text === 'false') {
-          type = 'trueFalse';
-          options = ['true', 'false'];
-        } else if (!item.Choice_1_text && !item.Choice_2_text && !item.Choice_3_text && !item.Choice_4_text) {
-          type = 'openResponse';
-          options = [];
-        } else {
-          type = 'multipleChoice';
-          options = [item.Choice_1_text, item.Choice_2_text, item.Choice_3_text, item.Choice_4_text].filter(Boolean);
-        }
-
-        return {
-          text: item.Question_text,
-          options: options,
-          type: type
-        };
-      });
-      setQuestions(newQuestions);
-      setAnswers(Array(newQuestions.length).fill(''));
+      try {
+        const dbresponse = await getAssessmentQuestions(assessmentId);
+        const formattedQuestions = dbresponse.data.questions.map(formatQuestion);
+        setQuestions(formattedQuestions);
+        setAnswers(Array(formattedQuestions.length).fill(''));
+        setShowPopups(Array(formattedQuestions.length).fill(false));
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
     };
+
     fetchData();
   }, [assessmentId]);
-  
+
+  const formatQuestion = (item) => {
+    let type, options;
+    if (item.Choice_1_text === 'true' && item.Choice_2_text === 'false') {
+      type = 'trueFalse';
+      options = ['true', 'false'];
+    } else if (!item.Choice_1_text && !item.Choice_2_text && !item.Choice_3_text && !item.Choice_4_text) {
+      type = 'openResponse';
+      options = [];
+    } else {
+      type = 'multipleChoice';
+      options = [item.Choice_1_text, item.Choice_2_text, item.Choice_3_text, item.Choice_4_text].filter(Boolean);
+    }
+
+    return { text: item.Question_text, options, type };
+  };
+
   const handleAnswerChange = (index, value) => {
-    const newAnswers = [...answers];
-    newAnswers[index] = value;
-    setAnswers(newAnswers);
+    setAnswers(answers => answers.map((answer, i) => i === index ? value : answer));
+  };
+
+  const togglePopup = (index) => {
+    setShowPopups(popups => popups.map((popup, i) => i === index ? !popup : popup));
   };
 
   const submitAnswers = () => {
-    // Logic to submit answers
     console.log(answers);
+    // Add submission logic here
   };
 
   return (
     <div className="Assessment">
-      {questions.map((question, index) => (
-        <div key={index} className="student-question">
-          <div className="question-text">{index + 1}. {question.text}</div>
-          {question.type === 'openResponse' ? (
-            <textarea 
-              value={answers[index]}
-              onChange={(e) => handleAnswerChange(index, e.target.value)}
+      <Switch isOn={isOn} handleToggle={() => setIsOn(!isOn)} />
+
+      {isOn ? (
+        <div className="canvas-questions">
+          {questions.map((question, index) => (
+            <QuestionItem
+              key={index}
+              question={question}
+              answer={answers[index]}
+              onAnswerChange={(value) => handleAnswerChange(index, value)}
             />
-          ) : (
-            <div className="options">
-              {question.options.map((option, optionIndex) => (
-                <label key={optionIndex}>
-                  <input
-                    type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
-                    name={`question-${index}`}
-                    value={option}
-                    checked={answers[index] === option}
-                    onChange={() => handleAnswerChange(index, option)}
-                  />
-                  {String.fromCharCode(97 + optionIndex)}) {option}
-                </label>
-              ))}
-            </div>
-          )}
+          ))}
         </div>
-      ))}
-      <button onClick={submitAnswers}>Submit Answers</button>
+      ) : (
+        <React.Fragment>
+          <div className="diagonal-buttons">
+            {questions.map((question, index) => (
+              <button className="question-button" onClick={() => togglePopup(index)} key={index}>
+                {question.text}
+              </button>
+            ))}
+          </div>
+          {questions.map((question, index) => (
+            <Popup
+              key={index}
+              question={question}
+              isOpen={showPopups[index]}
+              onClose={() => togglePopup(index)}
+              answer={answers[index]}
+              onAnswerChange={(value) => handleAnswerChange(index, value)}
+            />
+          ))}
+        </React.Fragment>
+      )}
+
+      <button
+        onClick={submitAnswers}
+        style={{ color: 'white', backgroundColor: '#007bff', border: 'none', padding: '10px 20px', borderRadius: '4px', cursor: 'pointer', marginTop: '20px' }}>
+        Submit Answers
+      </button>
     </div>
   );
 }
+
+const QuestionItem = ({ question, answer, onAnswerChange }) => {
+  return (
+    <div className="student-question">
+      <div className="question-text">{question.text}</div>
+      {question.type === 'openResponse' ? (
+        <textarea value={answer} onChange={(e) => onAnswerChange(e.target.value)} />
+      ) : (
+        <div className="options">
+          {question.options.map((option, optionIndex) => (
+            <label key={optionIndex}>
+              <input
+                type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
+                name={`question-${optionIndex}`}
+                value={option}
+                checked={answer === option}
+                onChange={() => onAnswerChange(option)}
+              />
+              { } {option}
+            </label>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Popup = ({ question, isOpen, onClose, answer, onAnswerChange }) => {
+  return (
+    <div className={`popup ${isOpen ? 'active' : ''}`}>
+      <div className="popup-content">
+        <div className="question-text">{question.text}</div>
+        {question.type === 'openResponse' ? (
+          <textarea value={answer} onChange={(e) => onAnswerChange(e.target.value)} />
+        ) : (
+          <div className="options">
+            {question.options.map((option, optionIndex) => (
+              <label key={optionIndex}>
+                <input
+                  type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
+                  name={`question-${optionIndex}`}
+                  value={option}
+                  checked={answer === option}
+                  onChange={() => onAnswerChange(option)}
+                />
+                { } {option}
+              </label>
+            ))}
+          </div>
+        )}
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
 
 export default Assessment;
--- client/src/views/Assessment/AssessmentPage.jsx
+++ client/src/views/Assessment/AssessmentPage.jsx
@@ -6,146 +6,149 @@ import './StudentAssessment/StudentAssessment.css';
 
 function Assessment() {
   const [questions, setQuestions] = useState([]);
-  const localQuestions = JSON.parse(localStorage.getItem('questions'));
   const [answers, setAnswers] = useState([]);
   const [isOn, setIsOn] = useState(true);
-  const [showPopups, setShowPopups] = useState(Array(questions.length).fill(false));
+  const [showPopups, setShowPopups] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-
-      const newQuestions = localQuestions.map(item => {
-        let type, options;
-        if (item.Choice_1_text === 'true' && item.Choice_2_text === 'false') {
-          type = 'trueFalse';
-          options = ['true', 'false'];
-        } else if (!item.Choice_1_text && !item.Choice_2_text && !item.Choice_3_text && !item.Choice_4_text) {
-          type = 'openResponse';
-          options = [];
-        } else {
-          type = 'multipleChoice';
-          options = [item.Choice_1_text, item.Choice_2_text, item.Choice_3_text, item.Choice_4_text].filter(Boolean);
-        }
-
-        return {
-          text: item.Question_text,
-          options: options,
-          type: type
-        };
-      });
-      setQuestions(newQuestions);
-      setAnswers(Array(newQuestions.length).fill(''));
-    };
-    fetchData();
+    const localQuestions = JSON.parse(localStorage.getItem('questions')) || [];
+    const formattedQuestions = localQuestions.map(formatQuestion);
+    setQuestions(formattedQuestions);
+    setAnswers(Array(formattedQuestions.length).fill(''));
+    setShowPopups(Array(formattedQuestions.length).fill(false));
   }, []);
 
+  const formatQuestion = (item) => {
+    let type, options;
+    if (item.Choice_1_text === 'true' && item.Choice_2_text === 'false') {
+      type = 'trueFalse';
+      options = ['true', 'false'];
+    } else if (!item.Choice_1_text && !item.Choice_2_text && !item.Choice_3_text && !item.Choice_4_text) {
+      type = 'openResponse';
+      options = [];
+    } else {
+      type = 'multipleChoice';
+      options = [item.Choice_1_text, item.Choice_2_text, item.Choice_3_text, item.Choice_4_text].filter(Boolean);
+    }
+
+    return { text: item.Question_text, options, type };
+  };
+
   const handleAnswerChange = (index, value) => {
-    const newAnswers = [...answers];
-    newAnswers[index] = value;
-    setAnswers(newAnswers);
+    setAnswers(answers => answers.map((answer, i) => i === index ? value : answer));
+  };
+
+  const togglePopup = (index) => {
+    setShowPopups(popups => popups.map((popup, i) => i === index ? !popup : popup));
   };
 
   const submitAnswers = () => {
-    // Logic to submit answers
     console.log(answers);
-  };
-  const openPopup = (index) => {
-    const newPopups = [...showPopups];
-    newPopups[index] = true;
-    setShowPopups(newPopups);
-  };
-
-
-
-  const closePopup = (index) => {
-    const newPopups = [...showPopups];
-    newPopups[index] = false;
-    setShowPopups(newPopups);
+    // Add submission logic here
   };
 
   return (
     <div className="Assessment">
       <NavBar isStudent={true} />
-      <div>
-        <Switch
-          isOn={isOn}
-          handleToggle={() => setIsOn(!isOn)}
-        />
-      </div>
-      {isOn && (
+      <Switch isOn={isOn} handleToggle={() => setIsOn(!isOn)} />
+
+      {isOn ? (
         <div className="canvas-questions">
-      {questions.map((question, index) => (
-        <div key={index} className="student-question">
-          <div className="question-text">{index + 1}. {question.text}</div>
-          {question.type === 'openResponse' ? (
-            <textarea 
-              value={answers[index]}
-              onChange={(e) => handleAnswerChange(index, e.target.value)}
+          {questions.map((question, index) => (
+            <QuestionItem
+              key={index}
+              question={question}
+              answer={answers[index]}
+              onAnswerChange={(value) => handleAnswerChange(index, value)}
             />
-          ) : (
-            <div className="options">
-              {question.options.map((option, optionIndex) => (
-                <label key={optionIndex}>
-                  <input
-                    type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
-                    name={`question-${index}`}
-                    value={option}
-                    checked={answers[index] === option}
-                    onChange={() => handleAnswerChange(index, option)}
-                  />
-                  {String.fromCharCode(97 + optionIndex)}) {option}
-                </label>
-              ))}
-            </div>
-          )}
+          ))}
         </div>
-      ))}
-          <button onClick={submitAnswers}>Submit Answers</button>
-        </div>
-      )}
-      {!isOn && (
+      ) : (
         <React.Fragment>
           <div className="diagonal-buttons">
             {questions.map((question, index) => (
-              <button className="question-button" onClick={() => openPopup(index)} key={index}>{question.text}</button>
+              <button className="question-button" onClick={() => togglePopup(index)} key={index}>
+                {question.text}
+              </button>
             ))}
           </div>
           {questions.map((question, index) => (
-            <div key={index} className={`popup ${showPopups[index] ? 'active' : ''}`}>
-              <div className="popup-content">
-                <div className="question-text">{index + 1}. {question.text}</div>
-                {question.type === 'openResponse' ? (
-                  <div className="options">
-                    <textarea
-                      value={answers[index]}
-                      onChange={(e) => handleAnswerChange(index, e.target.value)}
-                    />
-                  </div>
-                ) : (
-                  <div className="options">
-                    {question.options.map((option, optionIndex) => (
-                      <label key={optionIndex}>
-                        <input
-                          type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
-                          name={`question-${index}`}
-                          value={option}
-                          checked={answers[index] === option}
-                          onChange={() => handleAnswerChange(index, option)}
-                        />
-                        { } {option}
-                      </label>
-                    ))}
-                  </div>
-                )}
-                <button className="close-button" onClick={() => closePopup(index)}>Close</button>
-              </div>
-            </div>
+            <Popup
+              key={index}
+              question={question}
+              isOpen={showPopups[index]}
+              onClose={() => togglePopup(index)}
+              answer={answers[index]}
+              onAnswerChange={(value) => handleAnswerChange(index, value)}
+            />
           ))}
-          <button onClick={submitAnswers}>Submit Answers</button>
         </React.Fragment>
       )}
+
+      <button
+        onClick={submitAnswers}
+        style={{ color: 'white', backgroundColor: '#007bff', border: 'none', padding: '10px 20px', borderRadius: '4px', cursor: 'pointer', marginTop: '20px' }}>
+        Submit Answers
+      </button>
+      <footer className="footer">
+        </footer>
     </div>
   );
 }
+
+const QuestionItem = ({ question, answer, onAnswerChange }) => {
+  return (
+    <div className="student-question">
+      <div className="question-text">{question.text}</div>
+      {question.type === 'openResponse' ? (
+        <textarea value={answer} onChange={(e) => onAnswerChange(e.target.value)} />
+      ) : (
+        <div className="options">
+          {question.options.map((option, optionIndex) => (
+            <label key={optionIndex}>
+              <input
+                type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
+                name={`question-${optionIndex}`}
+                value={option}
+                checked={answer === option}
+                onChange={() => onAnswerChange(option)}
+              />
+              { } {option}
+            </label>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Popup = ({ question, isOpen, onClose, answer, onAnswerChange }) => {
+  return (
+    <div className={`popup ${isOpen ? 'active' : ''}`}>
+      <div className="popup-content">
+        <div className="question-text">{question.text}</div>
+        {question.type === 'openResponse' ? (
+          <textarea value={answer} onChange={(e) => onAnswerChange(e.target.value)} />
+        ) : (
+          <div className="options">
+            {question.options.map((option, optionIndex) => (
+              <label key={optionIndex}>
+                <input
+                  type={question.type === 'trueFalse' ? 'radio' : 'checkbox'}
+                  name={`question-${optionIndex}`}
+                  value={option}
+                  checked={answer === option}
+                  onChange={() => onAnswerChange(option)}
+                />
+                { } {option}
+              </label>
+            ))}
+          </div>
+        )}
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
 
 export default Assessment;
